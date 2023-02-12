41a80eb84f7058b5cf947a607cf5e860d6d9ff64
towhidulislamalif <towhidulislamalif@gmail.com>
2023-02-13 03:12:37 +0600

    some problem

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const chalk = require('chalk');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 
 // ? middleware
 app.use(cors());
